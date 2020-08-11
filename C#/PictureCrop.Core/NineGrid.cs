f69d97521f8e386a27389a41c073af3ab3f984cd
chenguang.li <lichenguang080621@gmail.com>
2020-08-12 00:14:15 +0800

    [Add]  提交C#版本的图片九切

---
+++ C#/PictureCrop.Core/NineGrid.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace PictureCrop.Core
+{
+    public static class NineGrid
+    {
+        public static void Crop(string sourcePath, string destPath, int left, int top, int right, int bottom, int centerW, int centerH)
+        {
+            Bitmap source = null;
+            Bitmap dest = null;
+            Graphics g = null;
+            try
+            {
+                source = new Bitmap(sourcePath);
+                int rawWidth = source.Width;
+                int rawHeight = source.Height;
+                dest = new Bitmap(left + right + centerW, top + bottom + centerH, source.PixelFormat);
+
+                g = Graphics.FromImage(dest);
+                g.InterpolationMode = InterpolationMode.NearestNeighbor;
+
+                //左上
+                g.DrawImage(source, new Rectangle(0, 0, left, top), new Rectangle(0, 0, left, top), GraphicsUnit.Pixel);
+                //左下
+                g.DrawImage(source, new Rectangle(0, top + centerH, left, bottom), new Rectangle(0, rawHeight - bottom, left, bottom), GraphicsUnit.Pixel);
+                //右上
+                g.DrawImage(source, new Rectangle(left + centerW, 0, right, top), new Rectangle(rawWidth - right, 0, right, top), GraphicsUnit.Pixel);
+                //右下
+                g.DrawImage(source, new Rectangle(left + centerW, top + centerH, right, bottom), new Rectangle(rawWidth - right, rawHeight - bottom, right, bottom), GraphicsUnit.Pixel);
+
+                //上
+                g.DrawImage(source, new Rectangle(left, 0, centerW, top), new Rectangle(left, 0, rawWidth - left - right, top), GraphicsUnit.Pixel);
+                //下
+                g.DrawImage(source, new Rectangle(left, top + centerH, centerW, bottom), new Rectangle(left, rawHeight - bottom, rawWidth - left - right, bottom), GraphicsUnit.Pixel);
+                //左
+                g.DrawImage(source, new Rectangle(0, top, left, centerH), new Rectangle(0, top, left, rawHeight - top - bottom), GraphicsUnit.Pixel);
+                //右
+                g.DrawImage(source, new Rectangle(left + centerW, top, right, centerH), new Rectangle(rawWidth - right, top, right, rawHeight - top - bottom), GraphicsUnit.Pixel);
+
+                //中
+                g.DrawImage(source, new Rectangle(left, top, centerW, centerH), new Rectangle(left, top, rawWidth - right - left, rawHeight - top - bottom), GraphicsUnit.Pixel);
+
+                dest.Save(destPath, source.RawFormat);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("PictureCrop NiceGrid Crop Error " + e);
+            }
+            finally
+            {
+                g?.Dispose();
+                dest?.Dispose();
+                source?.Dispose();
+            }
+        }
+    }
+
+}
---
+++ C#/PictureCrop.Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using PictureCrop.Core;
+
+namespace PictureCrop.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace(true);
+            System.Diagnostics.StackFrame sf = st.GetFrame(0);
+            string path = sf.GetFileName();
+
+            string sourceDir = Path.Combine(Path.GetDirectoryName(path), "SourcePicture");
+            string outputDir = sourceDir.Replace("SourcePicture", "OutputPicture");
+
+            if (Directory.Exists(outputDir))
+            {
+                Directory.Delete(outputDir, true);
+            }
+
+            Directory.CreateDirectory(outputDir);
+
+            string[] files = Directory.GetFiles(sourceDir);
+
+            foreach (var filePath in files)
+            {
+                string rawFileName = Path.GetFileNameWithoutExtension(filePath);
+
+                string[] paramStrings = rawFileName.Split('@', '_');
+
+                string fileName = paramStrings[0];
+
+                int left = int.Parse(paramStrings[1]);
+                int top = int.Parse(paramStrings[2]);
+                int right = int.Parse(paramStrings[3]);
+                int bottom = int.Parse(paramStrings[4]);
+                int centerW = int.Parse(paramStrings[5]);
+                int centerH = int.Parse(paramStrings[6]);
+
+                string outputPath = Path.Combine(outputDir, fileName + Path.GetExtension(filePath));
+
+                NineGrid.Crop(filePath, outputPath, left, top, right, bottom, centerW, centerH);
+            }
+        }
+    }
+}
